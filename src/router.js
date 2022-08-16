d50760b5d3542a30971505609be512d478b174d2
YOUR_USERNAME <stroegeorgian96@yahoo.com>
2022-08-16 23:07:55 +0300

    #5 - add update endpoint

--- src/router.js
+++ src/router.js
@@ -18,4 +18,16 @@ function configureRoutes(expressApp, projectModel) {
                 res.json(project["dataValues"]);
             }).catch((err) => res.status(404).send());
         });
+        // 5-Add an endpoint for updating an existing project based on its id
+        expressApp.put('/projects/:id', async function(req, res) {
+            const id = req.params.id;
+            await projectModel.update({ ...req.body  }, {
+                where: {
+                  id: id
+                }
+              });
+              res.status(200).send();
+          });
 }
+
+// Change everyone without a last name to "Doe"
