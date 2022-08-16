8c8cd594b670047d14d8a78f1a431a697425248d
PetreCarmen <102144661+PetreCarmen@users.noreply.github.com>
2022-08-16 23:53:37 +0300

    #5 - add an endpoint for updating an existing project based on its ID (#12)
    * #5 - add update endpoint
    
    * #5 - Fix update endpoint
    
    Co-authored-by: YOUR_USERNAME <stroegeorgian96@yahoo.com>

--- src/router.js
+++ src/router.js
@@ -1,12 +1,10 @@
 module.exports = {
     configureRoutes: configureRoutes
 }
+const express = require('express')
 
 function configureRoutes(expressApp, projectModel) {
-    expressApp.get('/hello-world', (req, res) => {
-        res.setHeader("Content-Type", "application/json");
-        res.json({message: 'Hello, World!'});
-    });
+    expressApp.use(express.json());
 
     //adding an endpoint which can return a project based on a given project id
     expressApp.get('/project/:id',
@@ -18,10 +16,22 @@ function configureRoutes(expressApp, projectModel) {
                 res.json(project["dataValues"]);
             }).catch((err) => res.status(404).send());
         });
+    // 5-Add an endpoint for updating an existing project based on its id
+    expressApp.put('/projects/:id', function (req, res) {
+        const id = req.params.id;
+        projectModel.update(req.body, {
+            where: {
+                ID: id
+            }
+        }).then(() => res.status(200).send())
+            .catch(err => {
+                console.log("could not update", err);
+                res.status(500).send();
+            });
+    });
 
     //adding a select all projects endpoint 
     expressApp.get('/projects', function(req, res){
         projectModel.findAll().then(projects =>res.json(projects));
     })
-
 }
