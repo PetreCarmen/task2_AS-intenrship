cfee3d804f37d90fb1b8c14031e3921d84e2746b
Stroe Georgian <stroegeorgian96@yahoo.com>
2022-08-22 23:11:38 +0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ function configureRoutes(expressApp, sequelize) {
     expressApp.use(express.json());
 
     configureProjectRoutes(expressApp, sequelize)
+    configureInterviewsRoutes(expressApp, sequelize)
 }
 
 function configureProjectRoutes(expressApp, sequelize) {
@@ -47,6 +48,64 @@ function configureProjectRoutes(expressApp, sequelize) {
     })
 }
 
+//Create read update delete for Interviuri
+
+function configureInterviewsRoutes(expressApp, sequelize){
+    
+    //adding a select all Interviews endpoint
+
+    //GET route for interviews
+    expressApp.get('/interviews', function(req, res){
+        sequelize.models.interview.findAll().then(interviews =>res.json(interviews));
+    })
+    //DELETE route for interviews
+    expressApp.delete('/interviews/:id', function(req, res) {
+        sequelize.models.interview.destroy({
+            where: {
+                ID: req.params.id
+            }
+        }).then(() => res.status(200).send())
+            .catch(() => res.status(500).send());
+    })
+    //POST route for interviews
+
+    //PUT route for interviews
+    expressApp.put('/interviews/:id', function (req, res) {
+        const id = req.params.id;
+        sequelize.models.candidate.update(req.body, {
+            where: {
+                ID: id
+            }
+        }).then(() => res.status(200).send())
+            .catch(err => {
+                console.log("could not update", err);
+                res.status(500).send();
+            });
+    })
+    expressApp.post ('/interview', function(req, res){
+        // Create an interview
+        const interview = {
+            Interview_date: req.body.Interview_date,
+            Interview_duration: req.body.Interview_duration,
+            Interviewer: req.body.Interviewer
+        };
+
+        // Save Interview in the database
+        interview.post(interview)
+          .then(data => {
+            res.send(data);
+          })
+          .catch(err => {
+            res.status(500).send({
+              message:
+                err.message || "Some error occurred while inserting candidate."
+            });
+          });
+      });
+
+ 
+    }
 module.exports = {
     configureRoutes: configureRoutes
 }
+
