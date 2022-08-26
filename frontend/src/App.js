f1ec6317d71e6b9ee04317f3ec1e6ee8f5d6e1d6
Magda Dascalu <magda.roca15@gmail.com>
2022-08-27 00:17:08 +0300

    app.js fixed again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,19 +20,8 @@ function App() {
             <Route path="/candidates" element={<Candidates />} />
           </Routes>
         </Router>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <TableExample/>
     </div>
   );
 }
